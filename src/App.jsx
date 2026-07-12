0918caab61daf4e6a0b351e89904e07b145d3448
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-12 07:59:20 +0300

    Add color selection using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const colors = ["red", "blue", "green", "yellow", "purple"];
 
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div className="app">
-      <h1>🎨 Color Palette Picker</h1>
+     <h1>🎨 Color Palette Picker</h1>
 
       <div className="palette">
         {colors.map((color, index) => (
@@ -13,9 +16,12 @@ function App() {
             key={index}
             className="color-box"
             style={{ backgroundColor: color }}
+            onClick={() => setSelectedColor(color)}
           ></div>
         ))}
       </div>
+
+      <h2>Selected Color: {selectedColor || "None"}</h2>
     </div>
   );
 }
