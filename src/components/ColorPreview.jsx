a284dd5ffa244d49e1c58a522fad495d5f0d5d19
Sumeya Arebo <sumeyaarebo94@gmail.com>
2026-07-12 08:37:04 +0300

    Add color preview component

--- src/components/ColorPreview.jsx
+++ src/components/ColorPreview.jsx
@@ -1,12 +1,29 @@
 function ColorPreview({ selectedColor, setSelectedColor }) {
   return (
-    <>
-      <h2>Selected Color: {selectedColor || "None"}</h2>
+    <div style={{ marginTop: "30px" }}>
+      {selectedColor ? (
+        <>
+          <div
+            style={{
+              width: "150px",
+              height: "150px",
+              backgroundColor: selectedColor,
+              margin: "0 auto",
+              border: "3px solid black",
+              borderRadius: "10px",
+            }}
+          ></div>
+
+          <h2>Selected Color: {selectedColor}</h2>
+        </>
+      ) : (
+        <h2>No color selected yet</h2>
+      )}
 
       <button onClick={() => setSelectedColor("")}>
         Reset
       </button>
-    </>
+    </div>
   );
 }
 
